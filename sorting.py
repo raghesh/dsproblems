ccf67d9a48c031bc1b122fff392c1d0db48320b7
Raghesh <raghesh.a@gmail.com>
2011-12-28 16:55:19 +0530

    Implemented Insertion Sort

--- sorting.py
+++ sorting.py
@@ -11,13 +11,28 @@ class Sorting:
     """
 
 class InsertSort(Sorting):
-  """Describe the logic of insertion sort here.
+  """Insertion sort can be compared to arranging playing cards in our left
+     hand. Each card is taken from the table and it is placed into its
+     position. Eg :- L = [5,| 1, 7, 2, 6]. Here elements left to the symbol
+     | are already sorted.
+     Step 1: key = 1, L = [1, 5,| 7, 2, 6]
+     Step 2: key = 7, L = [1, 5, 7,| 2, 6]
+     Step 3: key = 2, L = [1, 2, 5, 7,| 6]
+     Step 4: key = 6, L = [1, 2, 5, 6, 7|]
   """
 
   def __init__(self):
     self.sortedList = []
 
   def Sort(self, List):
+    for I in range(1, len(List)):
+      Key = List[I]
+      J = I - 1
+      while J >= 0 and Key < List[J]:
+        List[J+1] = List[J]
+        J = J - 1
+      List[J+1] = Key
+
     self.sortedList = List
     return self.sortedList
   
